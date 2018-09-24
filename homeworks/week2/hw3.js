f33db700388b032a725d9f220517780dfd0680a0
Peter Siao <a130142806@gmail.com>
2018-09-25 03:11:42 +0800

    w2hw3fix

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,11 +1,14 @@
 function isPrime(n) {
+	var controller = 0;
   	for (var i = 2; i <= n; i++ ) {
   		if (n%i == 0 && n != i) {
-  			return false;
+  			controller = 1;
   		}
-  		return true;
   	}
-  	return false;
+  	if (controller == 0 && n != 1) {
+  		return true;
+  	} 
+    return false;
 }
 
 module.exports = isPrime
