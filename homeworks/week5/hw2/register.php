c139630ce2230a06af3744e79bcad07b72bd8e6a
Peter Siao <a130142806@gmail.com>
2018-10-16 15:29:54 +0800

    formobile2

--- homeworks/week5/hw2/register.php
+++ homeworks/week5/hw2/register.php
@@ -13,15 +13,17 @@
 		<div class="pic2"></div>
 		<div class="container">
 			<div class="navBar">
-				<h class="navBar__title">いっらしゃいませ！歡迎光臨~★★★</h>
+				<h class="navBar__title">いらっしゃいませ！歡迎光臨~★★★</h>
 			</div>
 			<div class="comment-display">
 				<form class="comment__form login" action="register_checking.php" method="POST">
 					帳號 account :  <input class="inputarea" name="usernames" type="text" /><br>
 					密碼 password :  <input class="inputarea" name="passwords" type="password" /><br>
 					暱稱 nickname :  <input class="inputarea" name="nickname" type="text">
-					<input type="submit" class="button__2" value="註冊起來!"/>
-					<button class="button__2"><a href="login.php">我已經有帳號了!</a></button>
+					<div class="buttoncenter">
+						<input type="submit" class="button__2" value="註冊起來!"/>
+						<button class="button__2"><a href="login.php">我已經有帳號了!</a></button>
+					</div>
 				</form>
 			</div>
 		</div>
