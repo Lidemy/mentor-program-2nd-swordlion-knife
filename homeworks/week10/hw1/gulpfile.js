8028e683ae019fd85a6d2c631b400c996d87d791
Peter Siao <a130142806@gmail.com>
2018-11-21 07:01:10 +0800

    gulp

---
+++ homeworks/week10/hw1/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var clean = require('gulp-clean');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('styles',['clean'],()=> {
+    gulp.src('./source/*.scss')   
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false,
+            remove:true 
+        }))
+        .pipe(rename({
+        	suffix: '.min'
+        }))
+        .pipe(gulp.dest('./output'));
+});
+
+gulp.task('es5', ['clean'],()=> {
+	gulp.src('./source/*.js')
+		.pipe(babel({
+			"presets": ["es2015"]
+		}))
+        .pipe(uglify())
+        .pipe(rename({
+        	suffix: '.min'
+        }))
+        .pipe(gulp.dest('./output'));
+});
+
+gulp.task('clean', ()=>{
+	return gulp.src('./output/*')
+		.pipe(clean());
+});
