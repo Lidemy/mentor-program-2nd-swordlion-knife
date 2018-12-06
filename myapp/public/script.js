f0d26db7ee7cb77e28cb925354fc20a98ec4fcbb
Peter Siao <a130142806@gmail.com>
2018-12-06 19:15:50 +0800

    express done

--- myapp/public/script.js
+++ myapp/public/script.js
@@ -18,40 +18,7 @@ $(document).ready(() => {
 			window.location="login";
 		}
 	})
-	// 網路頁面導向 (換頁功能)
-	var actual_link = location.href;
-	var pagenum = actual_link.split('page=')[1];
-	var pagelength = document.querySelectorAll('.changePageContainer__page').length;
-	var page = document.querySelectorAll('.changePageContainer__page');
-	if(!pagenum) {
-		page[0].style = 'font-size:30px;';
-	} else {
-		page[pagenum-1].style = 'font-size:30px;';
-	}
-	$('.changePageContainer').on('mouseover','.page', e => {
-		for(var i = 0; i < pagelength; i++) {
-			document.querySelectorAll('.changePageContainer__page')[i].style = 'font-size:20px;';
-		}
-		e.target.style = 'font-size:30px;';
-	})
-	//這邊想用get method 把網址PO上去 但不用會QQ (問題~)
-	$('.changePageContainer').on('click','.changePageContainer__page',e => {
-		window.location.href="index.php?page=" + e.target.innerText;
-	})
-	$('.changePageContainer').on('click','.changePageContainer__previous',e => {
-		if(parseInt(pagenum) - 1 > 0) {
-			window.location.href="index.php?page=" + (parseInt(pagenum)-1);
-		}
-	})
-	$('.changePageContainer').on('click','.changePageContainer__next',e => {
-		if(parseInt(pagenum) + 1 <= pagelength) {
-			window.location.href="index.php?page=" + (parseInt(pagenum)+1);
-		} else if(!pagenum) {
-			window.location.href="index.php?page=2";
-		}
-	})
 })
-
 	// 這是網頁最下面的日期更新
 ShowTime = () => {
 　	var Today = new Date();
